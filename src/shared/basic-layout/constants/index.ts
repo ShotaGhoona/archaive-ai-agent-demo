aaafd89b36f7aa73969cc5ab8f3c54204318f377
ShotaGhoona <shota.yamashita@ghoona.com>
2025-08-08 13:13:02 +0900

    buildエラー修正

--- src/shared/basic-layout/constants/index.ts
+++ src/shared/basic-layout/constants/index.ts
@@ -1,3 +1,5 @@
 // Re-export all constants for easier imports
-export * from './navigation';
+export * from './header-navigation';
+export * from './blueprint-detail-navigation';
+export * from './customer';
 export * from './settings';
