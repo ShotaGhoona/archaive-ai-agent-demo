10e9a4fa8835f2aae5165afa093075349f4e6eb3
ShotaGhoona <shota.yamashita@ghoona.com>
2025-08-30 14:54:43 +0900

    図面一覧ページのデータ整理

---
+++ src/dummy-data/blueprint/index.ts
@@ -0,0 +1,5 @@
+// Blueprint dummy data exports
+export { default as blueprintHomeData } from './blueprint-home-data.json';
+
+// Blueprint interface exports
+export type { BlueprintHomeDataInterface } from './interfaces/types';
---
+++ src/dummy-data/blueprint/interfaces/types.ts
@@ -0,0 +1,17 @@
+export interface BlueprintHomeDataInterface {
+  filename: string;
+  orderSource: string;
+  productName: string;
+  internalNumber: string;
+  customerNumber: string;
+  cadName: string;
+  camName: string;
+  orderQuantity: number;
+  orderDate: string;
+  deliveryDate: string;
+  maxDimensionL: number;
+  maxDimensionD: number;
+  maxDimensionH: number;
+  companyField: string;
+  image: string;
+}
