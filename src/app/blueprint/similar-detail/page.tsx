7511e544decd91ebcce73f2c6079752a7cf954a8
ShotaGhoona <shota.yamashita@ghoona.com>
2025-11-15 16:14:57 +0900

    fix: build-error

--- src/app/blueprint/similar-detail/page.tsx
+++ src/app/blueprint/similar-detail/page.tsx
@@ -1,5 +1,19 @@
+import { Suspense } from 'react';
 import { BlueprintSimilarDetailContainer } from '@/page-components/blueprint/similar-detail/ui';
+import { Loading } from '@/shared';
 
 export default function BlueprintSimilarDetailPage() {
-  return <BlueprintSimilarDetailContainer />;
+  return (
+    <Suspense
+      fallback={
+        <Loading
+          title='読み込み中...'
+          description='ページを読み込んでいます'
+          fullHeight
+        />
+      }
+    >
+      <BlueprintSimilarDetailContainer />
+    </Suspense>
+  );
 }
